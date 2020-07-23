2691015ad2ef0b529125ef877125c8e4084ca5b7
Balamurali M <balamurali05@outlook.com>
2020-07-24 04:34:58 +0530

    ✨ Add view for location list and details

---
+++ inv_manager/locations.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+
+from .database import Location
+
+bp = Blueprint('locations', __name__, url_prefix='/locations')
+
+
+@bp.route('/')
+def list():
+    locations = Location.query.all()
+    return render_template('location_list.html', locations=locations)
+
+
+@bp.route('/<int:location_id>/')
+def detail(location_id):
+    location = Location.query.get(location_id)
+    return render_template('location_detail.html', location=location)
