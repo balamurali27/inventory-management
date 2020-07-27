2702bfc5d789622821eeaaa2bfc158baf08e1fcb
Balamurali M <balamurali05@outlook.com>
2020-07-27 06:49:03 +0530

    ✨ Implement product movement creation via forms

--- inv_manager/product_movements.py
+++ inv_manager/product_movements.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 
-from .database import ProductMovement
+from .database import Location, Product, ProductMovement, db
 
 bp = Blueprint('product_movements', __name__, url_prefix='/product_movements')
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def list():
+
+    if request.method == 'POST':
+        __import__('pprint').pprint(request.form)
+        from_location = Location.query.get_or_404(request.form['from_location'])
+        to_location = Location.query.get_or_404(request.form['to_location'])
+        product = Location.query.get_or_404(request.form['product'])
+        qty = request.form['qty']
+        product_movement = ProductMovement(from_location_id=from_location.id,
+                                           to_location_id=to_location.id,
+                                           product_id=product.id,
+                                           qty=qty)
+        db.session.add(product_movement)
+        db.session.commit()
+        return redirect(url_for('product_movements.detail',
+                                product_movement_id=product_movement.id))
+
     product_movements = ProductMovement.query.all()
     return render_template('product_movement_list.html',
-                           product_movements=product_movements)
+                           product_movements=product_movements,
+                           locations=Location.query.all(),
+                           products=Product.query.all())
 
 
 @bp.route('/<int:product_movement_id>/')
