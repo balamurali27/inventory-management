05d7d9a7327b484e30f40b3087163bd2eaadc2c6
Balamurali M <balamurali05@outlook.com>
2020-07-27 06:10:32 +0530

    ✨ Implement location creation via form

--- inv_manager/locations.py
+++ inv_manager/locations.py
@@ -1,12 +1,20 @@
 from flask import Blueprint, render_template
+from flask import request, redirect, url_for
 
-from .database import Location
+from .database import Location, db
 
 bp = Blueprint('locations', __name__, url_prefix='/locations')
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def list():
+    if request.method == 'POST':
+        name = request.form['name']
+        location = Location(name=name)
+        db.session.add(location)
+        db.session.commit()
+        return redirect(url_for('locations.detail', location_id=location.id))
+
     locations = Location.query.all()
     return render_template('location_list.html', locations=locations)
 
